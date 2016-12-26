d591d5f55dd120f56690f9199e938e2eebba4b30
卓修武 <nimdanoob@163.com>
2016-12-26 15:42:27 +0800

    delete android project

--- AndroidServer/app/src/main/java/zous/httpserver/IResourceUriHandler.java
+++
@@ -1,8 +0,0 @@
-package zous.httpserver;
-
-import java.io.IOException;
-
-public interface IResourceUriHandler {
-  boolean accept(String uri);
-  void handle(String uri,HttpContext httpContext) throws IOException;
-}
